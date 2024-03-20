036400d439211cefc5ceea3988df5bc5d077f80f
luhan <luhan@360.cn>
2024-03-20 14:05:44 +0800

    assets: add assets

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,10 +1,22 @@
 'use client'
 
+import Image from 'next/image'
+
 const Header = () => {
   return (
     <header className='h-20 w-full border-b-2 border-slate-200 px-4'>
-      <div className='lg:max-w-screen-lg max-auto flex items-center justify-between h-full bg-red-500'>
-        Header
+      <div className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-500'>
+        <div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
+          <Image
+            alt='Mascot'
+            src='/mascot.svg'
+            height={40}
+            width={40}
+          />
+          <h1 className='text-2xl font-extrabold text-green-600 tracking-wide'>
+            Lingo
+          </h1>
+        </div>
       </div>
     </header>
   )
