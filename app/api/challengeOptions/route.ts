74c27561f329c12634a3c34b23446194530183e0
luhan <luhan@360.cn>
2024-04-11 16:21:05 +0800

    feat: debug

--- app/api/challengeOptions/route.ts
+++ app/api/challengeOptions/route.ts
@@ -1,56 +1,29 @@
-import { NextResponse } from "next/server"
-import { eq } from "drizzle-orm"
+import { NextResponse } from "next/server";
 
-import db from "@/db/drizzle"
-import { challengeOptions } from "@/db/schema"
-import { isAdmin } from "@/lib/admin"
+import db from '@/db/drizzle'
+import { challengeOptions } from "@/db/schema";
+import { isAdmin } from "@/lib/admin";
 
-export const GET = async (
-  req: Request,
-  { params }: {
-    params: { challengeOptionId: number }
-  }
-) => {
+export const GET = async () => {
   if (!isAdmin()) {
-    return new NextResponse('Unauthorized', { status: 403 })
+    return new NextResponse('Unauthorized', { status: 401 })
   }
 
-  const data = await db.query.challengeOptions.findFirst({
-    where: eq(challengeOptions.id, params.challengeOptionId)
-  })
+  const data = await db.query.challengeOptions.findMany()
 
   return NextResponse.json(data)
 }
 
-export const PUT = async (
-  req: Request,
-  { params }: {
-    params: { challengeOptionId: number }
-  }
-) => {
+export const POST = async (req: Request) => {
   if (!isAdmin()) {
-    return new NextResponse('Unauthorized', { status: 403 })
+    return new NextResponse('Unauthorized', { status: 401 })
   }
 
   const body = await req.json()
-  const data = await db.update(challengeOptions).set({
+
+  const data = await db.insert(challengeOptions).values({
     ...body
-  }).where(eq(challengeOptions.id, params.challengeOptionId)).returning()
-
-  return NextResponse.json(data[0])
-}
-
-export const DELETE = async (
-  req: Request,
-  { params }: {
-    params: { challengeOptionId: number }
-  }
-) => {
-  if (!isAdmin()) {
-    return new NextResponse('Unauthorized', { status: 403 })
-  }
-
-  const data = await db.delete(challengeOptions).where(eq(challengeOptions.id, params.challengeOptionId)).returning()
+  }).returning()
 
   return NextResponse.json(data[0])
 }
